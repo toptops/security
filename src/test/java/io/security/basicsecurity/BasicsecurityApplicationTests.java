8493a12d8fdfb177e44a08ced24d7c6f0febc704
toptops <seongtab@naver.com>
2020-11-03 13:32:41 +0900

    first commit

---
+++ src/test/java/io/security/basicsecurity/BasicsecurityApplicationTests.java
@@ -0,0 +1,11 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsecurityApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
